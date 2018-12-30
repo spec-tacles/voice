8c5e495f895546a33b99cec6f125383e1a46ef7d
Will Nelson <will@pleb.xyz>
2018-12-29 23:17:39 -0800

    add root command

--- cmd/root.go
+++ cmd/root.go
@@ -2,18 +2,51 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 
+	"github.com/spec-tacles/voice/voice"
 	"github.com/spf13/cobra"
+)
+
+var (
+	endpoint  = new(string)
+	serverID  = new(uint64)
+	userID    = new(uint64)
+	sessionID = new(string)
+	token     = new(string)
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "voice",
 	Short: "A voice utility for Discord.",
+	Long: `Takes stdin and transmits to Discord. Piped data must be Opus audio packets with
+2 channels and 48kHz sample rate.`,
+	Run: func(c *cobra.Command, args []string) {
+		v := voice.New()
+		v.Connect(*endpoint, &voice.Identify{
+			ServerID:  *serverID,
+			SessionID: *sessionID,
+			UserID:    *userID,
+			Token:     *token,
+		})
+
+		_, err := io.Copy(v.UDP, os.Stdin)
+		if err != nil {
+			fmt.Println(err)
+		}
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(connectCmd)
+	rootCmd.Flags().StringVar(endpoint, "endpoint", "", "voice endpoint to connect to (VOICE_SERVER_UPDATE)")
+	rootCmd.Flags().Uint64Var(serverID, "server_id", 0, "guild ID of this voice connection (VOICE_STATE_UPDATE)")
+	rootCmd.Flags().Uint64Var(userID, "user_id", 0, "user ID of this voice connection (VOICE_STATE_UPDATE)")
+	rootCmd.Flags().StringVar(sessionID, "session_id", "", "session ID of this voice connection (VOICE_STATE_UPDATE)")
+	rootCmd.MarkFlagRequired("endpoint")
+	rootCmd.MarkFlagRequired("server_id")
+	rootCmd.MarkFlagRequired("user_id")
+	rootCmd.MarkFlagRequired("session_id")
 }
 
 // Execute the CLI app
